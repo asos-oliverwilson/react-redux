5fb342e47ac77e80bca873040f6e4a10d0cf31eb
Oliver Wilson <oliver.wilson@asos.com>
2018-01-09 15:34:36 +0000

    Completed section 7

--- src/components/App.js
+++ src/components/App.js
@@ -5,7 +5,7 @@ class App extends React.Component {
     render() {
         return(
             <div className="contianer-fluid">
-                <Header></Header>
+                <Header/>
                 {this.props.children}
             </div>
         );
