45987144cabbfc5d2396cac6fafd584c9fdbd8aa
Oliver Wilson <oliver.wilson@asos.com>
2018-01-11 11:55:50 +0000

    Async in Redux

--- src/components/courses/CoursesPage.js
+++ src/components/courses/CoursesPage.js
@@ -3,46 +3,23 @@ import PropTypes from 'prop-types';
 import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
 import * as courseActions from '../../actions/courseActions';
+import CourseList from './CourseList';
 
 class CoursesPage extends React.Component {
     constructor(props, context){
         super(props, context);
 
-        this.state = {
-            course: { title: "new course" }
-        };
-
-        this.onTitleChange = this.onTitleChange.bind(this);
-        this.onClickSave = this.onClickSave.bind(this);
-    }
-
-    onTitleChange(event){
-        const course = this.state.course;
-        course.title = event.target.value;
-        this.setState({ course: course });
-    }
-
-    onClickSave(){
-        this.props.actions.createCourse(this.state.course);
     }
 
     courseRow(course, index){
         return <div key={index}>{course.title}</div>;
     }
-    render() {        
+    render() {
+        const {courses} = this.props;
         return (
             <div>
                 <h1>Courses</h1>
-                {this.props.courses.map(this.courseRow)}
-                <h2>Add Course</h2>
-                <input
-                    type="text"
-                    onChange={this.onTitleChange}
-                    value={this.state.course.title} />
-                <input
-                    type="submit"                    
-                    value="Save"
-                    onClick={this.onClickSave} />                
+                <CourseList courses={courses}/>              
             </div>
         );
     }
@@ -71,6 +48,7 @@ function mapDispatchToProps(dispatch){
     }
 }
 */
+
 // Map all of the actions found in courseAction to a call to dispatch.
 function mapDispatchToProps(dispatch){
     return{
--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import configureStore from './store/configureStore';
 import { Provider } from 'react-redux';
 import { Router, browserHistory } from 'react-router';
 import routes from './routes';
+import { loadCourses } from './actions/courseActions';
 import './styles/styles.css';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 
@@ -12,6 +13,8 @@ import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 It serves just one purpose : to “provide” the store to its child components. */
 
 const store = configureStore();
+store.dispatch(loadCourses());
+ 
 render(
     <Provider store={store}> 
         <Router history={browserHistory} routes={routes} />
